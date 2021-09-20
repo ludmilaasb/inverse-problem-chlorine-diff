fdcf295e0acb3b4766ec2ef806bd69a95beeed11
Ludmila Botelho <ludmilaasb@gmail.com>
2021-09-21 00:34:36 +0200

    up, erf revised

--- least_squares.example.py
+++ least_squares.example.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 # print('Please, define data (data1,data2...data4) and ')
 dt = 'data2'
 if len(sys.argv)<2:
-    print("Data not specified. Using data2 as example for 3 months deposition")
+    print("Data not specified. Using data2 as example for 3 months deposition for the 1st term of gaussian error series")
 else:
     dt =sys.argv[1]
 data = pd.read_csv('data/{}.txt'.format(dt), sep=" ", header=None)
@@ -17,14 +17,21 @@ Y = data[1].values
 
 # this is equivalent to: alpha = np.linalg.lstsq(A, y, rcond=None)[0]
 # def get_alpha(X,Y):
-A = np.vstack([X, np.ones(len(X))]).T
+def A_erf_pol_exp(X, num_ex):
+    A = []
+    for i in range(num_ex):
+        A.append(X**(2*i+1))
+    A.append(np.ones(len(X)))
+    return np.vstack(A).T
+
+A = A_erf_pol_exp(X, 1)
 Y = Y[:, np.newaxis]
-alpha = np.dot((np.dot(np.linalg.inv(np.dot(A.T,A)),A.T)),Y) # can be also defined using pseudo-inverse, np.dot(np.linalg.inv(np.dot(A.T,A)),A.T)) = np.linalg.pinv(A)
-    # return alpha                                                 # but for the sake of understanding this method, better to show it explicit
+p = np.dot((np.dot(np.linalg.inv(np.dot(A.T,A)),A.T)),Y) # can be also defined using pseudo-inverse, np.dot(np.linalg.inv(np.dot(A.T,A)),A.T)) = np.linalg.pinv(A)
+    # return alpha                                       # but for the sake of understanding this method, better to show it explicit
 
 plt.figure(figsize = (10,8))
 plt.plot(X, Y, 'b.')
-plt.plot(X, alpha[0]*X + alpha[1], 'r')
+plt.plot(X, np.dot(A,p), 'r')
 plt.xlabel('x')
 plt.ylabel('y')
 # plt.show()
