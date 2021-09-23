731560ec69040f5ed52c319b5281ed8fa7245058
Ludmila Botelho <ludmilaasb@gmail.com>
2021-09-23 10:10:09 +0200

    up

--- least_squares.example.py
+++ least_squares.example.py
@@ -8,23 +8,23 @@ if len(sys.argv) == 2 and sys.argv[1] in ['-h','--help']:
     exit()
 
 dt = 'data2'
-time = 1
-num_ex = 1
+time = 2
+num_ex = 3
 
 if len(sys.argv)<2:
-    print("Data not specified. Using data2 as example for 3 months deposition for the 1st term of gaussian error series. In case of doubt, just use help (-h, --help)")
+    print("Data not specified. Using data2 as example for 6 months deposition for the 3rd term of gaussian error series. In case of doubt, just use help (-h, --help)")
 else:
     dt =sys.argv[1]
     assert dt in ['data1','data2','data3','data4'], "data must by like: 'data1','data2','data3','data4'"
     time = int(sys.argv[2])/3
-    assert time in [1,2] , 'time must be equal to 3 or 60'
+    assert time in [1,2] , 'time must be equal to 3 or 6'
     num_ex = int(sys.argv[3])
-    assert num_ex > 0 , "expassion term must be integer and positive    "
+    assert num_ex > 0 , "expanssion term must be integer and positive    "
 
 data = pd.read_csv('data/{}.txt'.format(dt), sep=" ", header=None)
-#Defining ‘X’ as independent variable and ‘Y’ as dependent variable
+# Defining ‘X’ as independent variable and ‘Y’ as dependent variable
 X = data[0].values
-Y = data[1].values
+Y = data[time].values
 
 # this is equivalent to: alpha = np.linalg.lstsq(A, y, rcond=None)[0]
 # def get_alpha(X,Y):
@@ -38,9 +38,17 @@ def A_erf_pol_exp(X, num_ex):
 A = A_erf_pol_exp(X, num_ex)
 
 Y = Y[:, np.newaxis]
-p = np.dot((np.dot(np.linalg.inv(np.dot(A.T,A)),A.T)),Y) # can be also defined using pseudo-inverse, np.dot(np.linalg.inv(np.dot(A.T,A)),A.T)) = np.linalg.pinv(A)
-    # return alpha                                       # but for the sake of understanding this method, better to show it explicit
+p = np.dot((np.dot(np.linalg.inv(np.dot(A.T,A)),A.T)),Y) # can be also defined using pseudo-inverse, in other words, np.dot(np.linalg.inv(np.dot(A.T,A)),A.T)) = np.linalg.pinv(A)
+                                                         # but for the sake of understanding this method, better to show it explicit
 
+# ------ Parameters estimation -------
+num_monts = 3*time
+month2seconds = num_monts*30*24*60*60
+
+D = p[-1][0]/(np.pi*(p[0][0]**2)*month2seconds)
+print('It seems that the answer is D = {} and rho={}'.format(D,p[-1][0]))
+
+# ------ Plotting -------
 plt.figure(figsize = (10,8))
 plt.plot(X, Y, 'b.')
 plt.plot(X, np.dot(A,p), 'r')
